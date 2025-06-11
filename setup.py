cb4675f5da8d0f59c90b8b530865c360ea7c270f
Chumicat <russell57260620@gmail.com>
2025-06-11 16:16:59 +0800

    Release: AuthFi API Python wrapper v0.0.3 Related URL patched on pypi: - Original URL on pypi is wrong, SECOND TIME modified. I HATE YOU   CACHE!! - Hope This time will work, since it work on test pypi.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="authfi-api",
-    version="0.0.2",
+    version="0.0.3",
     author="chumicat",
     author_email="russell57260620@gmail.com",
     description="Python wrapper for Authentrend AuthFi WebAuthn/FIDO2 API",
